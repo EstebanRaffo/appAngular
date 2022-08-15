77c8f75089757db72dc02b4e11b515a79683ae22
Esteban Raffo <e.fraffo@gmail.com>
2022-08-15 17:22:44 -0300

    15/08

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -10,13 +10,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegistroComponent implements OnInit {
   // private fb:FormBuilder = new FormBuilder()
   myForm:FormGroup
+
   constructor(private fb:FormBuilder) {
     this.myForm = this.fb.group({
-      nombre:["", [Validators.required, Validators.minLength(3)]],
+      nombre:["", [Validators.required]],
       apellido:["", [Validators.required]],
       // correo:["",[Validators.required, Validators.pattern(/^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/)]],
+      telefono:["", []],
       correo:["", [Validators.required]],
-      clave:["", [Validators.required]]
+      clave:["", [Validators.required, Validators.minLength(6), Validators.maxLength(10)]]
     })
    }
   
